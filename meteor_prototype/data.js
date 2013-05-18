4fb4ae65e5235f4d8e0d20b48963349c2d51f302
st-a <albert.steinmetz@gmail.com>
2013-05-18 16:25:30 +0200

    beispielhafter Datensatz

---
+++ meteor_prototype/data.js
@@ -0,0 +1,6 @@
+data = {
+    "tasks": [
+        {"name":"Task0", "percent_completed":0.5, "percent_average_completition_time":0.8},
+        {"name":"Task1", "percent_completed":0.2, "percent_average_completition_time":0.3},
+    ]
+};
