96d88b3cbc49fd438b75a424bc8caf0bab02dcfb
st-a <albert.steinmetz@gmail.com>
2013-05-18 23:10:20 +0200

    Tasks dynamisch aus JSON erzeugen und in Spalten anordnen

--- meteor_prototype/data.js
+++ meteor_prototype/data.js
@@ -1,6 +1,18 @@
 data = {
-    "tasks": [
-        {"name":"Task0", "percent_completed":0.5, "percent_average_completition_time":0.8},
-        {"name":"Task1", "percent_completed":0.2, "percent_average_completition_time":0.3},
+
+    "columns": [
+        {"text":"Entwerfen", "tasks": [
+            {"text":"Backend", "percent_completed":0.2, "percent_average_completition_time":0.3},
+            ]
+        },
+        {"text":"Umsetzen", "tasks": [
+                // empty
+            ]
+        },
+        {"text":"Ausliefern", "tasks": [
+            {"text":"Interface", "percent_completed":0.5, "percent_average_completition_time":0.8},        
+            {"text":"Storage", "percent_completed":0.6, "percent_average_completition_time":0.7},        
+            ]},
     ]
+
 };
--- meteor_prototype/meteor_prototype.js
+++ meteor_prototype/meteor_prototype.js
@@ -1,12 +1,15 @@
 ////// GLOBAL
 
-var renderSampleTask = function(percent_completed, percent_average_completition_time) {
+// wie viele Spalten gibt es?
+var column_count = data.columns.length;
 
-  //var percent_completed = 0, percent_average_completition_time = 0;
+// einen Task rendern
+var renderTask = function(text, percent_completed, percent_average_completition_time) {
+
   var task_width = 200, task_height = 100;
 
   // Zeichenfläche erzeugen
-  var sampleTaskSVG = d3.select("#sample_task")
+  var sampleTaskSVG = d3.select("#" + text)
   .append("svg")
   .attr("width", task_width)
   .attr("height", task_height);
@@ -30,7 +33,7 @@ var renderSampleTask = function(percent_completed, percent_average_completition_
   sampleTaskSVG.append("polygon")
   .style("stroke", "none")
   .style("fill", "#00a99d")
-  .attr("points", "0,0 0," + task_height + " " + (task_width * percent_completed) + "," + task_height);
+  .attr("points", "0,0 0," + task_height + " " +  (task_width * percent_completed) + "," + task_height);
 }
 
 ////// CLIENT
@@ -39,19 +42,24 @@ if (Meteor.isClient) {
 
   // wird ausgeführt, sobald das Dokument geladen ist
   Meteor.startup(function() {
-    // console.log(data.tasks[0].name);
+    // console.log(column_count);
   });
   
-  // wenn div #sample_task angezeigt wird, Task rendern
-  Template.task.rendered = function() {
-    renderSampleTask(data.tasks[0].percent_completed, data.tasks[0].percent_average_completition_time);
+  // div-Elemente im DOM erzeugen
+  Template.board.columns = function() {
+    return data.columns;
   }
+
+  // jedes mal, wenn das task-template geladen wird, entsprechendes SVG einfügen
+  Template.task.rendered = function() {
+    renderTask(this.data.text, this.data.percent_completed, this.data.percent_average_completition_time);
+  };
 }
 
 ////// SERVER
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
