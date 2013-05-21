30f71f1c93f8e440791fc777ac98f0d7ff860e3d
TomGeissler <Tom.Geissler@tu-dresden.de>
2013-05-21 21:21:04 +0200

    Verschiebung eines Tasks nur in die nächste Spalte nötig
    Da ist noch die Schleife kaputt um den task unter den letzten der
    nächsten Spalte einzuordnen.

--- taskboard_alpha/taskboard_alpha.js
+++ taskboard_alpha/taskboard_alpha.js
@@ -1,6 +1,9 @@
 //globale Var
 var task_width = 200, task_height = 100;
-var mTask;
+var tbX,tbY;
+
+//Hilfsvariablen für DandD
+var mTask, oldX, oldY;
 
 //Button zum spielen
 var update = function(){
@@ -11,6 +14,9 @@ var update = function(){
 function start(id) {
 //Mousepos innerhalb eines Tasks  
 mTask = d3.mouse($('#' + id)[0]);
+
+ oldX = $('#' + id).position().left;
+ oldY = $('#' + id).position().top;
 
 }
 
@@ -28,7 +34,30 @@ var dragTarget = d3.select('#' + id);
 };
 
 //Funktion beim Drop
-function stop() {
+function stop(t) {
+ var posX = $('#' + t.id).position().left;
+ 
+  if((posX < (oldX+(task_width+10)-task_width/3)) || (posX > oldX+(2*task_width+10)))
+  {
+    d3.select('#' + t.id).transition().attr("transform", "translate(" + (oldX-tbX) + "," + (oldY-tbY) +")").duration(1000).ease("elastic");
+  }
+   
+   
+  //die Schleife ist noch kaputt  
+  else{
+    for (var task in dataset){
+      alert(dataset.state);
+      if ((t.state == (dataset.state +1)) && (dataset.after == null) ) {
+        afterTask = dataset.id;
+        alert(afterTask);
+      } 
+    }
+    
+    
+   d3.select('#' + t.id).transition().attr("transform", "translate(" + (oldX+task_width+10) + "," + (oldY-tbY) +")").duration(600);  
+  //funktion zum aendern der vorgaenger/nachfolger und nachschieben
+    
+  }  
 
 }
 
@@ -43,7 +72,7 @@ var position = function(state, before){
     y = 0;
   }
   else {
-    y = $('#' + before).position().top + task_height-20 ;
+    y = $('#' + before).position().top + task_height+ 10 - tbY ;
   } 
   return "translate(" + x + "," + y + ")";
 }
@@ -68,7 +97,7 @@ d3.select("#taskboard").selectAll("g")
     .call(d3.behavior.drag()
     .on("dragstart", function(d){start(d.id)})     
     .on("drag", function(d){move(d.id)})
-    .on("dragend", stop))
+    .on("dragend", function(d){stop(d)}))
     
     //Rechteck
     .append("rect")
@@ -91,6 +120,8 @@ if (Meteor.isClient) {
  
   
    Template.board.rendered = function() {
+     tbX = $('#taskboard').position().left;
+     tbY = $('#taskboard').position().top;
      renderTask();
   };
    
