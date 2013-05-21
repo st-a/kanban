7507723d12488195eb4936d5c414aa440d8f372d
TomGeissler <Tom.Geissler@tu-dresden.de>
2013-05-21 16:47:11 +0200

    taskposition in SVG
    positionierung der Tasks bezüglich ihres States und dem Vorgänger(falls
    vorhanden)

--- taskboard_alpha/taskboard_alpha.js
+++ taskboard_alpha/taskboard_alpha.js
@@ -1,19 +1,85 @@
-if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to taskboard_alpha.";
-  };
+var task_width = 200, task_height = 100;
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+
+var update = function(){
+  d3.selectAll("g").transition().attr("transform", "translate(100,100").duration(1000);
 }
 
+var position = function(state, before){
+  //alert(d);
+  var y, x;
+  
+  x = state*(task_width+20);
+  
+  //alert(before);
+  if (before == null) {
+    y = 0;
+  }
+  
+  else {
+  y = $('#' + before).position().top + task_height-20 ;
+  }
+  
+  return "translate(" + x + "," + y + ")";
+  
+}
+
+
+
+var renderTask = function() {
+var datad = [ 5, 10, 15, 20, 25 ];
+
+
+
+var Sample
+
+  //// Reihenfolge und Farben festlegen
+  d3.select("#taskboard").selectAll("g")
+
+.data(dataset.task).enter()
+
+    .append("g")
+    .attr("id" , function(d){return d.id})
+    .attr("transform", function(d){return position(d.state, d.before)})
+    .append("rect")
+    .attr("height", task_height)
+    .attr("width", task_width)
+    .attr("fill", "red");
+    
+
+
+
+  //// SVG in DOM einfügen
+
+  // Zeichenfläche erzeugen
+
+}
+
+
+////// CLIENT
+
+if (Meteor.isClient) {
+
+  Meteor.startup(function() {
+     
+    $('button').click(update);
+  });
+  
+
+ 
+  
+   Template.board.rendered = function() {
+     renderTask();
+  };
+   
+}
+
+////// SERVER
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
+  
+  
